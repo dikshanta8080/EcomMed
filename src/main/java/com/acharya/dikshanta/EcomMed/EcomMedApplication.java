fe1169a10185c48a87107b232b74905a3f92ebd4
Dikshanta Acharya <dikshantaacharya04@gmail.com>
2026-05-30 08:38:35 +0545

    Initialized the Project

---
+++ src/main/java/com/acharya/dikshanta/EcomMed/EcomMedApplication.java
@@ -0,0 +1,13 @@
+package com.acharya.dikshanta.EcomMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomMedApplication.class, args);
+	}
+
+}
