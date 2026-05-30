318c79bd9cdd36d1764d5b78eaff0c7a314a8e57
Dikshanta Acharya <dikshantaacharya04@gmail.com>
2026-05-30 08:50:56 +0545

    Confiigure Database and Hikari cp

---
+++ src/main/java/com/acharya/dikshanta/EcomMed/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.acharya.dikshanta.EcomMed.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping
+    public ResponseEntity<String> getHealthStatus(HttpServletRequest request, HttpServletResponse response) {
+        String message = String.format("Tomcat is running in 8080 with SessionId: %s", request.getSession().getId());
+        return ResponseEntity.ok(message);
+    }
+}
