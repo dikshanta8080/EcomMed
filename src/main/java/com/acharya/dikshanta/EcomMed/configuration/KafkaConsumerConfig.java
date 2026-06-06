63f3a38136c858877679e08e2e09af21032a0e0d
Dikshanta Acharya <dikshantaacharya04@gmail.com>
2026-06-06 21:27:08 +0545

    Configure Consumer Factory

---
+++ src/main/java/com/acharya/dikshanta/EcomMed/configuration/KafkaConsumerConfig.java
@@ -0,0 +1,28 @@
+package com.acharya.dikshanta.EcomMed.configuration;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+    @Value(("${Kafka.bootstrap-servers}"))
+    private String bootstrapServer;
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerConfig() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+}
--- src/main/java/com/acharya/dikshanta/EcomMed/configuration/KafkaProducerConfig.java
+++ src/main/java/com/acharya/dikshanta/EcomMed/configuration/KafkaProducerConfig.java
@@ -1,6 +1,5 @@
 package com.acharya.dikshanta.EcomMed.configuration;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
